7d07ae79bf955e55a7fec513a88819d3faf71033
Ringo Sham <ringosham@gmail.com>
2019-10-03 16:32:04 +0100

    Fixed credit message

--- src/main/java/com/ringosham/translationmod/common/ChatUtil.java
+++ src/main/java/com/ringosham/translationmod/common/ChatUtil.java
@@ -32,7 +32,7 @@ public class ChatUtil {
 
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     public static void printCredits() {
-        String version = (String) ModList.get().getModContainerById("translationmod").get().getModInfo().getModProperties().get("version");
+        String version = ModList.get().getModContainerById("translationmod").get().getModInfo().getVersion().toString();
         ChatUtil.printChatMessage(false, "Real-time translation mod by Ringosham. Version " + version, TextFormatting.AQUA);
         ChatUtil.printChatMessage(false, "Online translation services powered by Yandex and Google", TextFormatting.AQUA);
         ChatUtil.printChatMessage(false, "Translation results may not be 100% accurate", TextFormatting.AQUA);
