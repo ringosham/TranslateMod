e6f4fa3b1d8a03ca77dfef348ed004ca0a457535
Ringo Sham <ringosham@gmail.com>
2020-09-04 13:56:55 +0100

    Preparing 5.1.1 release

--- src/main/java/com/ringosham/translationmod/client/GooglePaidClient.java
+++ src/main/java/com/ringosham/translationmod/client/GooglePaidClient.java
@@ -7,16 +7,12 @@ import com.ringosham.translationmod.client.types.RequestResult;
 import com.ringosham.translationmod.client.types.google.TranslateError;
 import com.ringosham.translationmod.client.types.google.TranslateResponse;
 import com.ringosham.translationmod.common.ConfigManager;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GooglePaidClient {
     //This is the Google cloud translation API service
@@ -40,46 +36,47 @@ public class GooglePaidClient {
     }
 
     public RequestResult translate(String message, Language from, Language to) {
-        HttpClient client = HttpClientBuilder.create().build();
-        RequestBuilder builder = RequestBuilder.post();
-        builder.setUri(baseUrl);
+        Map<String, String> queryParam = new HashMap<>();
+        String encodedMessage = null;
+        //Percent encode message
+        try {
+            encodedMessage = URLEncoder.encode(message, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException ignored) {
+        }
         //The query supports multiline using an array, however we only have one line to text to translate.
+        queryParam.put("q", encodedMessage);
         //Query parameters
-        builder.addParameter("q", message);
-        builder.addParameter("target", to.getGoogleCode());
-        //Interpret text as... text. Apparently it defaults to HTML, what
-        builder.addParameter("format", "text");
-        builder.addParameter("key", ConfigManager.INSTANCE.getUserKey());
+        queryParam.put("target", to.getGoogleCode());
+        //Interpret text as... text. Apparently it defaults to HTML. What
+        queryParam.put("format", "text");
+        queryParam.put("key", ConfigManager.INSTANCE.getUserKey());
         //Skip the source language for auto detection
         if (from != LangManager.getInstance().getAutoLang()) {
-            builder.addParameter("source", from.getGoogleCode());
+            queryParam.put("source", from.getGoogleCode());
         }
-        HttpUriRequest request = builder.build();
-        try {
-            HttpResponse response = client.execute(request);
-            InputStream in = response.getEntity().getContent();
-            String responseString = IOUtils.toString(in, StandardCharsets.UTF_8);
-            Gson gson = new Gson();
-            if (response.getStatusLine().getStatusCode() == 200) {
-                TranslateResponse transResponse = gson.fromJson(responseString, TranslateResponse.class);
-                //Just get the first element. Since this is a single element query there should not be more than 1 entry in the array
-                String translatedText = transResponse.getData().getTranslations()[0].getTranslatedText();
-                Language sourceLang = LangManager.getInstance().findLanguageFromGoogle(transResponse.getData().getTranslations()[0].getDetectedSourceLanguage());
-                if (sourceLang == null)
-                    sourceLang = from;
-                return new RequestResult(200, translatedText, sourceLang, to);
-            } else {
-                try {
-                    TranslateError error = gson.fromJson(responseString, TranslateError.class);
-                    return new RequestResult(error.getError().getCode(), error.getError().getMessage(), null, null);
-                } catch (JsonSyntaxException e) {
-                    //Unknown response
-                    return new RequestResult(2, "Unknown response: " + responseString, null, null);
-                }
+        RESTClient.Response response = RESTClient.INSTANCE.POST(baseUrl, queryParam);
+        String responseString = response.getEntity();
+        Gson gson = new Gson();
+        if (response.getResponseCode() == 200) {
+            TranslateResponse transResponse = gson.fromJson(responseString, TranslateResponse.class);
+            //Just get the first element. Since this is a single element query there should not be more than 1 entry in the array
+            String translatedText = transResponse.getData().getTranslations()[0].getTranslatedText();
+            Language sourceLang = LangManager.getInstance().findLanguageFromGoogle(transResponse.getData().getTranslations()[0].getDetectedSourceLanguage());
+            if (sourceLang == null)
+                sourceLang = from;
+            return new RequestResult(200, translatedText, sourceLang, to);
+        } else {
+            //Internal errors
+            if (response.getResponseCode() < 100) {
+                return new RequestResult(response.getResponseCode(), response.getEntity(), null, null);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new RequestResult(1, "Connection error", null, null);
+            try {
+                TranslateError error = gson.fromJson(responseString, TranslateError.class);
+                return new RequestResult(error.getError().getCode(), error.getError().getMessage(), null, null);
+            } catch (JsonSyntaxException e) {
+                //Unknown response
+                return new RequestResult(2, "Unknown response: " + responseString, null, null);
+            }
         }
     }
 }
